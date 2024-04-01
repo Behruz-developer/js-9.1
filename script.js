ab1eb9eb79045ab7c7971719f5a701c73843873c
Behruz-developer <88540036+Behruz-developer@users.noreply.github.com>
2024-04-01 19:57:59 +0500

    finish

---
+++ script.js
@@ -0,0 +1,22 @@
+let str = 'Lorem, ipsum dolor, sit amet consectetur, adipisicing, elit. Ut deserunt, voluptatum, veritatis similique natus odit!'
+let arr = str.split('')
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i] == 'a' ) {
+        arr.splice(i, 1)
+    }
+    if (arr[i] == 'o'){
+        arr.splice(i, 1)
+    }
+    if (arr[i] == 'u'){
+        arr.splice(i, 1)
+    }
+    if (arr[i] == 'i'){
+        arr.splice(i, 1)
+    }
+    if (arr[i] == 'e'){
+        arr.splice(i, 1)
+    }
+}
+let string = arr.join('')
+console.log(string);
+
